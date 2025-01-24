e713feeae6a22cba2c10a729932f2852ec903745
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2025-01-25 00:12:39 +0530

    fixed css

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -3,7 +3,7 @@ import { IMG_CDN_URL } from "../Utils/constant"
 const MovieCard = ({posterPath}) => {
   if(!posterPath) return null;
   return (
-    <div className='m-2 flex'>
+    <div className='m-2 flex hover:scale-105'>
       <img
         className='min-w-[100px] max-w-[100px] md:min-w-[200px] md:max-w-[200px] h-auto object-cover rounded-md shadow-lg'
         src={IMG_CDN_URL + posterPath}
